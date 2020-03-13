d01ed41e576ea4c9a620d76baf0ecf8a7848732c
XingZiLong <41220208+XingZiLong@users.noreply.github.com>
2020-03-13 09:52:07 +0800

    Update registry-packages.ts

--- registry-packages.ts
+++ registry-packages.ts
@@ -115,4 +115,16 @@ export const packages: PackageInfo[] = [
     },
     aliases: ['fujing'],
   },
+
+  {
+    name: '連加連乘',
+    repo: 'XingZiLong/continuous-operation',
+    description: 'Do continuous addition and mutiplication',
+    author: {
+      name: 'XingZiLong',
+      url: 'https://github.com/XingZiLong',
+    },
+    aliases: ['continuous-operation'],
+  },
+
 ]
